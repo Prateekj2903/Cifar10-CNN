28472524bb63f5823be3f718d79c365005b8e13a
Prateek Jain <prateek.jain2903@gmail.com>
2018-01-10 01:59:18 +0530

    Initial Upload

---
+++ Load_Data.py
@@ -0,0 +1,41 @@
+import pickle
+import os
+import numpy as np
+path = "C:/Users/windo/Desktop/New folder/DataSets/cifar-10-batches-py"
+data = os.listdir(path)
+
+def load_train_data():
+    images = np.zeros((1, 3072))
+    labels = np.zeros(1, 'int32')
+    for file in data[1:6]:
+        with open(path+'/'+file, 'rb') as fo:
+            dict = pickle.load(fo, encoding='bytes')
+            images = np.vstack((images, dict[b'data']))
+            labels = np.hstack((labels, dict[b'labels']))
+    images = images[1:]
+    images = images.reshape(images.shape[0], 3, 32, 32)
+    images = images.transpose(0, 2, 3, 1)
+    images = images / 255.0
+    labels = labels[1:]
+    
+    return images, labels
+
+def load_test_data():
+    images = np.zeros((1, 3072))
+    labels = np.zeros(1, 'int32')
+    with open(path + '/' + data[len(data)-1], 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+        images = np.vstack((images, dict[b'data']))
+        labels = np.hstack((labels, dict[b'labels']))
+    images = images[1:]
+    images = images.reshape(images.shape[0], 3, 32, 32)
+    images = images.transpose(0, 2, 3, 1)
+    images = images / 255.0
+    labels = labels[1:]
+
+    return images, labels
+
+def get_labels():
+    with open(path + '/' + data[0], 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict[b'label_names']
